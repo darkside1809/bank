de392b170b1bec5d910c096912ef2f446c691893
Nekruz <njpnek@gmail.com>
2021-03-31 20:58:09 +0500

    add types and go.mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/darkside1809/bank
+
+go 1.16
---
+++ pkg/types/types.go
@@ -0,0 +1,49 @@
+package types
+
+
+// Money is amount of money in minimal currency amount like: panny, dirams etc.
+type Money int
+
+// Category is a category that has payment (cars, restaurants and others)
+type Category string
+
+// Currency is currency codes
+type Currency string
+
+
+// currency codes
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+//PAN is number of card
+type PAN string
+
+
+// Card is information about payment card
+type Card struct {
+	ID				int
+	PAN			PAN
+	Balance		Money
+	MinBalance 	Money
+	Currency 	Currency
+	Color			string
+	Name			string
+	Active		bool
+}
+
+//Payment is info about payment
+type Payment struct {
+	ID 			int
+	Amount 		Money
+	Category 	Category
+}
+
+// PaymentSource info about card
+type PaymentSource struct {
+	Type string
+	Number string
+	Balance Money
+}
